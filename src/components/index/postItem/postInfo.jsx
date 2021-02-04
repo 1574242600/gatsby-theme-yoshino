90bdb04b60b496e88f26d7481219d91cfb5c10d0
Nworm <1574242600@qq.com>
2021-02-04 14:08:40 +0800

    feat: 完成tag索引链接

--- src/components/index/postItem/postInfo.jsx
+++ src/components/index/postItem/postInfo.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "gatsby";
 import Time from "./postInfo/time";
 import PostInfoStyle from "./style/postInfo.module.css";
 
 export default function PostInfo(props) {
     const { date, update, timeToRead, tags } = props.info;
+
+    function renderTagLinks() {
+        if (tags == null) { return "null"; }
+        return tags.map(
+            (tag, index) => (
+                <React.Fragment key={ tag }>
+                    <Link to={`/tag/${ tag }`}>{ tag }</Link> 
+                    { tags[index + 1] === undefined ? "" : ", " }
+                </React.Fragment>
+            )
+        );
+    }
+
     return (
         <div className={ PostInfoStyle.postInfo }>
-            <Time time={{date, update}}/>
-            <span>{`预计阅读时长: ${timeToRead} 分钟`}</span>
-            |<span>{`标签: ${ tags ? tags.join(",") : "null" }`}</span>
+            <Time time={ { date, update } } />
+            <span>{ `预计阅读时长: ${timeToRead} 分钟` }</span>
+            |<span>{ "标签: " } { renderTagLinks() }</span>
 
         </div>
     );
