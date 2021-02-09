b730d5ced0b7daea2f90e494bb85cfb141556428
Nworm <1574242600@qq.com>
2021-02-09 18:51:24 +0800

    feat: 完成友链页面

--- src/pages/link.jsx
+++ src/pages/link.jsx
@@ -4,21 +4,37 @@ import { graphql } from "gatsby";
 import LinkStyle from "./style/link.module.css";
 
 export default class Link extends React.Component {
-    constructor(props) {
+    constructor (props) {
         super(props);
     }
 
-    renderFriend() {
-
+    renderFriends(links) {
+        return links.map((friend, index) => {
+            const { avatar, description, name, url, inaccessible } = friend;
+            return (
+                <li className={ LinkStyle.linkItem } key={ index }>
+                    <a href={url} target={"_blank"} rel="noreferrer">
+                        <img className={ "lazyload" } data-src={ avatar } />
+                        <h4>{ name }</h4>
+                        <p>{ description }</p>
+                        { inaccessible === true && 
+                            <p style={{ color: "red"}}> 链接已失效 </p>
+                        }
+                    </a>
+                </li>
+            );
+        });
     }
 
     render() {
         const { dataJson: { links } } = this.props.data;
 
         return (
-            <div className={ LinkStyle.center }>
-                
-                { JSON.stringify(links)}
+            <div className={ LinkStyle.linkCard }>
+                <h1>朋友们</h1>
+                <ul className={ LinkStyle.linkList }>
+                    { this.renderFriends(links) }
+                </ul>
             </div>
         );
     }
