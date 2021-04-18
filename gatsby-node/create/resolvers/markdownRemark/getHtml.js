cce7dc5bc6b64b8a14619b2fa87cc7170872e9fc
Nworm <1574242600@qq.com>
2021-04-18 11:37:27 +0800

    feat: 修改 marked renderer 图片懒加载

--- gatsby-node/create/resolvers/markdownRemark/getHtml.js
+++ gatsby-node/create/resolvers/markdownRemark/getHtml.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
 const marked = require("marked");
+const { cleanUrl } = require("marked/src/helpers");
 const { formatId } = require(require.resolve("../../../utils/index"));
 
 const renderer = {
@@ -8,6 +9,20 @@ const renderer = {
               <h${level} id="${formatId(text)}">
                 ${text}
               </h${level}>`;
+    },
+
+    image(href, title, text) {
+        href = cleanUrl(this.options.sanitize, this.options.baseUrl, href);
+        if (href === null) {
+            return text;
+        }
+
+        let out = "<img class=\"lazyload\" data-src=\"" + href + "\" alt=\"" + text + "\"";
+        if (title) {
+            out += " title=\"" + title + "\"";
+        }
+        out += this.options.xhtml ? "/>" : ">";
+        return out;
     }
 };
 
@@ -17,3 +32,5 @@ module.exports = async function getHTML(markdownBody) {
 
     return marked(markdownBody);
 };
+
+
